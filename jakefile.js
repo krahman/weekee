8a6b81c97566185681af51a0ee5fad295d76a855
khal <khal.rahman@gmail.com>
2014-12-14 20:18:05 +0800

    enabled node style linting

--- jakefile.js
+++ jakefile.js
@@ -1,14 +1,25 @@
 /**
  * Created by nevda on 14/12/14.
  */
+/*global desc, task, jake*/
+
+"use strict";
+
+
 task("default", ["lint"]);
 
 desc("lint everything");
 task("lint", [], function() {
-    var lint = require("./build/lint/lint_runner.js");
 
+    var lint = require("./build/lint/lint_runner.js");
     var files = new jake.FileList();
+
     files.include("**/*.js");
-    files.exclude(["build", "node_modules"]);
-    lint.validateFileList(files.toArray(), {}, {});
+    files.exclude(["node_modules"]);
+
+    var options = {
+        node: true
+    };
+
+    lint.validateFileList(files.toArray(), options, {});
 });
